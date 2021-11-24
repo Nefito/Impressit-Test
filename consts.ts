fff48502bba67e2de08b91acafc0e4191f59d74a
Nefito <vkorzhov00@gmail.com>
2021-11-24 12:57:43 +0200

    Move movieGenres in a separate file

---
+++ consts.ts
@@ -0,0 +1,63 @@
+export const movieGenres: { name: string, url: string }[] = [
+  {
+    name: 'Action & Adventures',
+    url: 'action__adventure_movies',
+  },
+  {
+    name: 'Animation',
+    url: 'animation_movies',
+  },
+  {
+    name: 'Classics',
+    url: 'classics_movies',
+  },
+  {
+    name: 'Comedy',
+    url: 'comedy_movies',
+  },
+  {
+    name: 'Documentary',
+    url: 'documentary_movies',
+  },
+  {
+    name: 'Drama',
+    url: 'drama_movies',
+  },
+  {
+    name: 'Kids & Family',
+    url: 'kids__family_movies',
+  },
+  {
+    name: 'Musical & Performing Arts',
+    url: 'musical__performing_arts_movies',
+  },
+  {
+    name: 'Mystery & Suspense',
+    url: 'mystery__suspense_movies',
+  },
+  {
+    name: 'Romance',
+    url: 'romance_movies',
+  },
+  {
+    name: 'Science Fiction & Fantasy',
+    url: 'science_fiction__fantasy_movies',
+  },
+  {
+    name: 'Special Interest',
+    url: 'special_interest_movies',
+  },
+  {
+    name: 'Sport & Fitness',
+    url: 'sports__fitness_movies',
+  },
+  {
+    name: 'Television',
+    url: 'television_movies',
+  },
+  {
+    name: 'Western',
+    url: 'western_movies',
+  },
+
+]
